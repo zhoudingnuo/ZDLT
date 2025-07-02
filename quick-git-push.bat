6086f6453b4162db805037bf7d9b93b8a8032250
zhoudingnuo <zhoudingnuo@szvt.com>
2025-07-03 00:37:19 +0800

    auto commit: 2025-07-03 0-37

--- quick-git-push.bat
+++ quick-git-push.bat
@@ -1,5 +1,5 @@
 @echo off
-REM 一键提交并推送到GitHub，并远程服务器自动pull（需手动输入密码）
+REM One-click commit, push to GitHub, and remote server git pull (manual password input required)
 
 git add .
 for /f "tokens=1-4 delims=/ " %%i in ("%date%") do set mydate=%%i-%%j-%%k
@@ -8,17 +8,17 @@ set msg=auto commit: %mydate% %mytime%
 git commit -m "%msg%"
 git push
 
-set SERVER_IP=你的服务器IP地址
+set SERVER_IP=47.107.84.24
 set SERVER_USER=root
 set SERVER_PATH=/root/ZDLT
 
 echo.
-echo ====== 现在将连接服务器并执行 git pull ======
-echo 你需要手动输入服务器密码：ZDLT@20250702
+echo ====== Connecting to server and running git pull ======
+echo Please manually enter the server password: ZDLT@20250702
 echo.
 
-REM 打开新命令行窗口执行ssh，pull完自动关闭
-start cmd /k "ssh %SERVER_USER%@%SERVER_IP% \"cd %SERVER_PATH% && git pull\" & echo. & echo 操作完成，按任意键关闭窗口 & pause & exit"
+REM Open a new command window to run ssh and auto-close after pull
+start cmd /k "chcp 65001 >nul && echo. && ssh %SERVER_USER%@%SERVER_IP% \"cd %SERVER_PATH% && git pull\" & echo. & echo Done. Press any key to close this window. & pause & exit"
 
-echo 已自动提交并推送到GitHub，并弹窗远程pull到服务器: %msg%
+echo Local commit and push to GitHub done. Remote git pull triggered: %msg%
 pause

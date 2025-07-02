9e8c25267403a7721537dc797ffd89f48a7628db
zhoudingnuo <zhoudingnuo@szvt.com>
2025-07-03 00:54:55 +0800

    auto commit: 2025-07-03 0-54

---
+++ 一键代码更新.bat
@@ -0,0 +1,23 @@
+@echo off
+REM One-click commit, push to GitHub, and remote server git pull (manual password input required)
+
+git add .
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do set mydate=%%i-%%j-%%k
+for /f "tokens=1-2 delims=: " %%i in ("%time%") do set mytime=%%i-%%j
+set msg=auto commit: %mydate% %mytime%
+git commit -m "%msg%"
+git push
+
+set SERVER_IP=47.107.84.24
+set SERVER_USER=root
+set SERVER_PATH=/root/ZDLT
+
+echo.
+echo ====== Connecting to server and running git pull ======
+echo Please manually enter the server password: 
+echo.
+
+ssh root@47.107.84.24 "cd %SERVER_PATH% && git pull"
+
+echo Local commit and push to GitHub done. Remote git pull triggered: %msg%
+pause

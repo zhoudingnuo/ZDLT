e2b68fc412d2ed54b5af457e40d50f5f5bf4489c
zhoudingnuo <zhoudingnuo@szvt.com>
2025-07-07 19:38:52 +0800

    auto commit: 2025-07-07 19-38

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2682,7 +2682,7 @@ body[data-theme="dark"] .markdown-body tr:nth-child(even) td {
                               fontSize: 12,
                               padding: '2px 8px'
                             }}>
-                              金额: ¥{msg.price !== null ? Number(msg.price).toFixed(4)+ getAgentBaseFee(agent?.id).toFixed(4) : '--'}
+                              金额: ¥{msg.price !== null ? Number(msg.price).toFixed(4) : '--'}
                             </span>
                           )}
                         </div>
